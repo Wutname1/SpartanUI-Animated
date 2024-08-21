e74a08f620c69040d09789762e51531a56032882
wutname1 <jeremy12@gmail.com>
2024-08-20 23:23:31 -0500

    Add Localization files

---
+++ lang/frFR.lua
@@ -0,0 +1,6 @@
+local L = LibStub('AceLocale-3.0'):NewLocale('SpartanUI-Animated', 'frFR')
+if not L then
+	return
+end
+
+--@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
